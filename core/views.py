d4aa4886b6eda7103ea495912b700e5630d9236f
ceciliaporcidonio <ceciliaporcidonio@gmail.com>
2024-11-28 17:01:18 -0300

    Adicionado botão para acessar a página de comentários

--- core/views.py
+++ core/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CommentForm
 
 def index(request):
     return render(request, 'core/index.html')
+
+def comment_view(request):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('comment')
+    else:
+        form = CommentForm()
+    return render(request, 'core/comment.html', {'form': form})
 
